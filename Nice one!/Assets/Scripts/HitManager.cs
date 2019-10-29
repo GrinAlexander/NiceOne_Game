c031492595a0dc6db66f73e1cce020271ad14251
GrinAlexander <elirhard@mail.ru>
2019-10-30 01:43:09 +0300

    Added movement of the ball with AddForce
    ...finally

--- Nice one!/Assets/Scripts/HitManager.cs
+++ Nice one!/Assets/Scripts/HitManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitManager : MonoBehaviour
+/*public class HitManager : MonoBehaviour
 {
     Rigidbody rb;
     public int count;
@@ -43,8 +43,7 @@ public class HitManager : MonoBehaviour
         {
             count++;
             isKicked = false;
-            rb.useGravity = true;
-            rb.AddForce(CalcQuadraticBezierPoint(1f, p0.position, p1.position, p2.position) * 20f);
+            rb.useGravity = true; 
         }
     }
 
@@ -59,25 +58,123 @@ public class HitManager : MonoBehaviour
         yield return new WaitForEndOfFrame();
         if (index < numPoints + 1)
         {
-            rb.MovePosition(CalcT());
+            rb.MovePosition(CalcQuadraticBezierPoint(p0.position, p1.position, p2.position));
+            
             index++;
             isKicked = true;
+            
         }
         else
         {
+            rb.AddForce(CalcQuadraticBezierPoint(p0.position, p1.position, p2.position) * 20f);
             index = 1;
             isKicked = false;
         }
     }
 
-    private Vector3 CalcT()
+    private float CalcT()
     {
-        float t = index / (float)numPoints;
-        return CalcQuadraticBezierPoint(t, p0.position, p1.position, p2.position);
+        return index / (float)numPoints;
     }
 
-    private Vector3 CalcQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    private Vector3 CalcQuadraticBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2)
     {
+        float t = CalcT();
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        Vector3 p = uu * p0;
+        p += 2 * u * t * p1;
+        p += tt * p2;
+        return p;
+    }
+}*/
+
+public class HitManager : MonoBehaviour
+{
+    Rigidbody rb;
+    public int count;
+    public Transform p0, p1, p2;
+    public bool isKicked = false;
+    private int numPoints = 50;
+    private float index = 1;
+    private Vector3 current;
+    private Vector3 next;
+    private float force = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.position == p1.position)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce((p2.position - p1.position) * force);
+
+        }
+        /* else if (rb.position.Equals(p2.position))
+         {
+             rb.useGravity = true;
+             isKicked = false;
+         }*/
+        /* if (isKicked && (index < numPoints + 1))
+         {
+
+             index++;
+             isKicked = true;
+
+         }
+         else
+         {
+             rb.AddForce(CalcQuadraticBezierPoint(p0.position, p1.position, p2.position) * 20f);
+             index = 1;
+             isKicked = false;
+         }*/
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Net"))
+        {
+            rb.useGravity = true;
+            isKicked = false;
+            count++;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        //current = rb.position;
+
+        rb.useGravity = false;
+        rb.AddForce(p1.position * force);
+        isKicked = true;
+    }
+
+    /* IEnumerator MoveWithDelay()
+     {
+         yield return new WaitForEndOfFrame();
+
+     }*/
+
+    private float CalcT()
+    {
+        return index / (float)numPoints;
+    }
+
+    private Vector3 CalcQuadraticBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float t = CalcT();
         float u = 1 - t;
         float tt = t * t;
         float uu = u * u;
